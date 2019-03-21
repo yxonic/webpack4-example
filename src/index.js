fdc42f474bf1cd1eee908c7d6281bc6964b86377
Yu Yin <yxonic@gmail.com>
2019-03-21 11:42:02 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as d3 from 'd3';
+(async function () {
+  // Selecting and appending elements
+  d3.select('#root')
+    .append('h5')
+    .append('text')
+    .text(`D3 version: ${d3.version}`)
+  // Loading external data
+  const dataset = await d3.csv('data/sample.csv')
+  dataset.forEach((data) => {
+    console.log(data)
+  })
+}) ();
